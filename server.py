9556e08c92a3ad2d467ed0897956dd62bc25a695
akulasrileela <akulasrileela@gmail.com>
2026-09-24 10:27:57 +0530

    Prepare Flask app for deployment

--- server.py
+++ server.py
@@ -1,18 +1,6 @@
 from flask import Flask, request, redirect, render_template, send_from_directory
 import sqlite3
 import os
-from flask import Flask
-from flask_mysqldb import MySQL
-
-app = Flask(__name__)
-
-app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
-app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
-app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
-app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
-app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
-
-mysql = MySQL(app)
 
 # =========================================================
 # FLASK SETUP
